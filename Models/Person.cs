4b3dc36e56c1ebcc19969ad8ab0479df85c5af42
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-09-09 12:05:56 -0300

    Person name is now required.

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -9,6 +10,7 @@ namespace people_web_api.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public int Age { get; set; }
