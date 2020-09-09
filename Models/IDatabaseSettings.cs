5d99bae99d99da85edf019ba94589b92eb7a8eee
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-09-09 14:48:15 -0300

    IDatabaseSettings docs.

--- Models/IDatabaseSettings.cs
+++ Models/IDatabaseSettings.cs
@@ -1,9 +1,26 @@
 namespace people_web_api.Models
 {
+    /// <summary>
+    /// Interface containing the necessary attributes for a runtime database setup.
+    /// </summary>
     public interface IDatabaseSettings
     {
-        string CollectionName { get; set; }
+        /// <summary>
+        /// Connection string to determine the client.
+        /// </summary>
+        /// <value></value>
         string ConnectionString { get; set; }
+
+        /// <summary>
+        /// Name of the database inside the client.
+        /// </summary>
+        /// <value></value>
         string DatabaseName { get; set; }
+
+        /// <summary>
+        /// Name of the specific collection inside the database.
+        /// </summary>
+        /// <value></value>
+        string CollectionName { get; set; }
     }
 }
